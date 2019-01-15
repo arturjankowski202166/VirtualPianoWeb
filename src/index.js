00eb96798dbd300a52b25f0d264fc66cca30298d
Artur Jankowski <ajankowski@ptc.com>
2019-01-15 09:49:55 +0100

    added comment field

--- src/index.js
+++ src/index.js
@@ -18,19 +18,27 @@ function loadRecordings()
 		for (let i = 0; i < result.length; i++)
 		{
 			console.log(result[i].email);
-			document.getElementById("recordings").innerHTML = '<div class="media border p-3"><div class="media-body"><h2>' + result[i].name + '</h2><br><h4>By '+ result[i].email + '</h4></div><audio controls><source src="http://localhost:5000/getFile/'+result[i].name+'.wav" type="audio/wav">Sum ting wong</audio></div>';
-			
+			document.getElementById("recordings").innerHTML += '<div class="media border p-3"><div class="media-body"><h2>' + result[i].name + '</h2><br><h4>By '+ result[i].email + '</h4></div><audio controls><source src="http://localhost:5000/getFile/'+result[i].name+'.wav" type="audio/wav">Sum ting wong</audio></div>';
+			addCommentField();
 		}
 	});
 }
 
-function getRequest(path)
+function addCommentField()
 {
+  document.getElementById("recordings").innerHTML += `<div class="status-upload">
+									<form>
+										<textarea placeholder="What are you doing right now?" ></textarea>
+										<ul>
+											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Audio"><i class="fa fa-music"></i></a></li>
+											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Video"><i class="fa fa-video-camera"></i></a></li>
+											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Sound Record"><i class="fa fa-microphone"></i></a></li>
+											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Picture"><i class="fa fa-picture-o"></i></a></li>
+										</ul>
+										<button type="submit" class="btn btn-success green"><i class="fa fa-share"></i> Share</button>
+									</form>
+								</div>"`
 }
-
-function postRequest(path)
-{
-	}
 
 window.onload = function()
 {
