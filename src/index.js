5f3636398fc5d6efe8e053b1bf599dc229ca7fa5
Artur Jankowski <ajankowski@ptc.com>
2019-01-17 14:29:55 +0100

    More things in layout

--- src/index.js
+++ src/index.js
@@ -11,33 +11,42 @@ function component() {
 
 function loadRecordings()
 {
-	document.getElementById("recordings").innerHTML = '<div class="jumbotron">test</div>';
-	$.getJSON('http://localhost:5000/getFileList', function(result)
+	//$.getJSON('http://localhost:5000/getFileList', function(result)
 	{
-		console.log(result);
-		for (let i = 0; i < result.length; i++)
+		//console.log(result);
+		//for (let i = 0; i < result.length; i++)
+    for (let i = 0; i < 2; i++)
 		{
-			console.log(result[i].email);
-			document.getElementById("recordings").innerHTML += '<div class="media border p-3"><div class="media-body"><h2>' + result[i].name + '</h2><br><h4>By '+ result[i].email + '</h4></div><audio controls><source src="http://localhost:5000/getFile/'+result[i].name+'.wav" type="audio/wav">Sum ting wong</audio></div>';
+			//console.log(result[i].email);
+      let tempUsr = "example@example.com"
+      let tempName = "exampleName"
+			//document.getElementById("recordings").innerHTML += '<div class="media border p-3"><div class="media-body"><h2>' + result[i].name + '</h2><br><h4>By '+ result[i].email + '</h4></div><audio controls><source src="http://localhost:5000/getFile/'+result[i].name+'.wav" type="audio/wav">Sum ting wong</audio></div>';
+      document.getElementById("recordings").innerHTML += `
+        <div class="border p-3">
+            <h2>` + tempName + `</h2>
+            <br>
+            <h4>By` + tempUsr + `</h4>
+          <div class="p-2">
+        <audio controls>`
+        +/*<source src="http://localhost:5000/getFile/'+tempName+'.wav" type="audio/wav">Sum ting wong*/`
+        </audio>`+
+        `</div>
+        <h4>Comments:</h4>`+
 			addCommentField();
 		}
-	});
+	}//);
 }
 
 function addCommentField()
 {
-  document.getElementById("recordings").innerHTML += `<div class="status-upload">
-									<form>
-										<textarea placeholder="What are you doing right now?" ></textarea>
-										<ul>
-											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Audio"><i class="fa fa-music"></i></a></li>
-											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Video"><i class="fa fa-video-camera"></i></a></li>
-											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Sound Record"><i class="fa fa-microphone"></i></a></li>
-											<li><a title="" data-toggle="tooltip" data-placement="bottom" data-original-title="Picture"><i class="fa fa-picture-o"></i></a></li>
-										</ul>
-										<button type="submit" class="btn btn-success green"><i class="fa fa-share"></i> Share</button>
-									</form>
-								</div>"`
+  return(
+            `<div class="form-group">
+									<textarea class="form-control" placeholder="Please comment..." ></textarea>
+                  <br>
+									<button type="submit" class="btn btn-success green"><i class="fa fa-share"></i> Share</button>
+							</div>
+            </div>
+          </div></div>`);
 }
 
 window.onload = function()
